7ef06ac8563a46a30579b01cc2a563680bef11e5
Rajasekaran121982 <138992967+Rajasekaran121982@users.noreply.github.com>
2023-07-14 16:50:50 +0200

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     recaptcha: {
-        siteKey: '6LeFrxcnAAAAADMJPixdURmMCKwR2B_uZmy1EKrL',
+        siteKey: '6LeFxxxxxxxxx',
     },
 };
 // const { Storage } = require('@google-cloud/storage');
